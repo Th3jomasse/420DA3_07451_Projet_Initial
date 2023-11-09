ff561d495f7c413a60a39aa5f19308c0b953c16e
GuillaumeGerv <guillaume.gerv5171@gmail.com>
2023-11-09 14:36:46 -0500

    Ajout Properties dans les DTO de Role et User

--- DataAccess/DTOs/RoleDTO.cs
+++ DataAccess/DTOs/RoleDTO.cs
@@ -7,4 +7,11 @@ using System.Threading.Tasks;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 public class RoleDTO : AbstractDTO<int> {
+
+    public string RoleName { get; set; } = null!;
+    public string RoleDescription { get; set; } = null!;
+
+    public const int NAME_MIN_LENGTH = 4;
+    public const int NAME_MAX_LENGTH = 64;
+    public const int ROLEDESCRIPTION_MAX_LENGTH = 256;
 }
--- DataAccess/DTOs/UserDTO.cs
+++ DataAccess/DTOs/UserDTO.cs
@@ -7,4 +7,10 @@ using System.Threading.Tasks;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 public class UserDTO : AbstractDTO<int> {
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public DateTime? DateCreation { get; set; }
+
+    public const int NAME_MIN_LENGTH = 4;
+    public const int NAME_MAX_LENGTH = 64;
 }
