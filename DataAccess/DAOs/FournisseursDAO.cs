678d3b3acebbc6bb30f77113bd6f66242d1b9bc5
Jonathan M. Massé <jonathan.m_masse@lcieducation.net>
2023-11-09 13:50:52 -0500

    Ajout Classe : ProduitsDAO Ajout Classe : FournisseursDAO Ajout Classe : ProduitsDTO Ajout Classe : FournisseursDTO

---
+++ DataAccess/DAOs/FournisseursDAO.cs
@@ -0,0 +1,27 @@
+﻿using _420DA3_07451_Projet_Initial.DataAccess.Contexts.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DAOs.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_07451_Projet_Initial.DataAccess.DAOs;
+public class FournisseursDAO : AbstractDao<FournisseursDTO, int> {
+    protected override AbstractContext Context { get; }
+
+    public FournisseursDAO(AbstractContext context) : base() {
+        this.Context = context;
+    }
+
+    public override FournisseursDTO Update(FournisseursDTO instance) {
+        instance.DateUpdated = DateTime.Now;
+        return base.Update(instance);
+    }
+
+    public override FournisseursDTO Delete(FournisseursDTO instance) {
+        instance.DateDeleted = DateTime.Now;
+        return base.Delete(instance);
+    }
+}
