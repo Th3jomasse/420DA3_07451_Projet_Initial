d7a995fb044e003dd6030cfd4b822754c290a083
Samuel <s.vonkuma@gmail.com>
2023-11-10 13:47:14 -0500

    Ajouter des informations dans AdressDTO

--- DataAccess/DTOs/AddressDTO.cs
+++ DataAccess/DTOs/AddressDTO.cs
@@ -9,5 +9,9 @@ namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 internal class AddressDTO : AbstractDTO<int> {
     public int ID { get; set; }
     public int CivicNumber { get; set; }
-    public string 
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string Country { get; set; }
+    public string PostalCode { get; set; }
 }
