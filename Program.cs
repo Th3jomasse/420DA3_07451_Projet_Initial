6e6ed7b548456f47936d5e891529785125985a1c
Th3Jomasse <jonathanm.masse@gmail.com>
2023-11-28 08:28:32 -0500

    Creation des launchs test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using _420DA3_07451_Projet_Initial.Business;
+using _420DA3_07451_Projet_Initial.Presentation;
 
 namespace _420DA3_07451_Projet_Initial;
 
@@ -7,7 +8,30 @@ internal static class Program {
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
-    public static void Main() {
-        new ExampleApp().Start();
+    public static void Main(string[] args) {
+        if (args.Contains("-jonathanTest")) {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new ProduitsManagementForm());
+        }
+
+        else if (args.Contains("-guillaumeTest")) {
+
+        }
+
+        else if (args.Contains("-reneclaudeTest")) {
+           
+        }
+
+        else if (args.Contains("-samuelTest")) {
+            
+        }
+
+        else if (args.Contains("-louayTest")) {
+            
+        } 
+
+        else {
+            new ExampleApp().Start();
+        }
     }
 }
