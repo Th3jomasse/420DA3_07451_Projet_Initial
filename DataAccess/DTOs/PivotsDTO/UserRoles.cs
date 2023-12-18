21769d156ad889575c4f6276816f1a78f395a060
Th3Jomasse <jonathanm.masse@gmail.com>
2023-12-18 16:29:01 -0500

    Changes 18/12/23 AppDBContext + Add Navigation + Add PivotsTable = Add public List<UserRoles> UserRoles { get; set; } = new List<UserRoles>(); in RoleDTO Add public List<UserRoles> UserRoles { get; set; } = new List<UserRoles>(); in UserDTO

--- DataAccess/DTOs/PivotsDTO/UserRoles.cs
+++ DataAccess/DTOs/PivotsDTO/UserRoles.cs
@@ -6,4 +6,9 @@ using System.Threading.Tasks;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs.PIvots;
 public class UserRoles {
+
+    public int UserId { get; set; }
+    public UserDTO User { get; set; } = null!;
+    public int RoleId { get; set; }
+    public RoleDTO Role { get; set; } = null!;
 }
