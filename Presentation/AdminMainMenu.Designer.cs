9d3d88b0e23728dc0929b9621d896e5f82c6fa49
Marc-Eric Boury <meboury@hotmail.com>
2023-12-18 19:12:45 -0500

    AdminMainMenu - section addresses

--- Presentation/AdminMainMenu.Designer.cs
+++ Presentation/AdminMainMenu.Designer.cs
@@ -25,10 +25,25 @@ partial class AdminMainMenu {
     /// </summary>
     private void InitializeComponent() {
         this.bottomBarPanel = new Panel();
-        this.topBarPanel = new Panel();
-        this.tableLayoutPanel1 = new TableLayoutPanel();
         this.buttonQuit = new Button();
+        this.topBarPanel = new Panel();
+        this.mainPanel = new TableLayoutPanel();
+        this.addressManagementPanel = new Panel();
+        this.addressMgmtMainPanel = new Panel();
+        this.existingAddressesLabel = new Label();
+        this.createAddressButton = new Button();
+        this.addressMgmtTopBar = new Panel();
+        this.label1 = new Label();
+        this.addressSearchTextBox = new TextBox();
+        this.filteredAddresses = new ListBox();
+        this.viewAddressButton = new Button();
+        this.editAddressButton = new Button();
+        this.deleteAddressButton = new Button();
         this.bottomBarPanel.SuspendLayout();
+        this.mainPanel.SuspendLayout();
+        this.addressManagementPanel.SuspendLayout();
+        this.addressMgmtMainPanel.SuspendLayout();
+        this.addressMgmtTopBar.SuspendLayout();
         this.SuspendLayout();
         // 
         // bottomBarPanel
@@ -40,31 +55,6 @@ partial class AdminMainMenu {
         this.bottomBarPanel.Size = new Size(1184, 40);
         this.bottomBarPanel.TabIndex = 0;
         // 
-        // topBarPanel
-        // 
-        this.topBarPanel.Dock = DockStyle.Top;
-        this.topBarPanel.Location = new Point(0, 0);
-        this.topBarPanel.Name = "topBarPanel";
-        this.topBarPanel.Size = new Size(1184, 40);
-        this.topBarPanel.TabIndex = 1;
-        // 
-        // tableLayoutPanel1
-        // 
-        this.tableLayoutPanel1.ColumnCount = 5;
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-        this.tableLayoutPanel1.Dock = DockStyle.Fill;
-        this.tableLayoutPanel1.Location = new Point(0, 40);
-        this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-        this.tableLayoutPanel1.RowCount = 2;
-        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-        this.tableLayoutPanel1.Size = new Size(1184, 781);
-        this.tableLayoutPanel1.TabIndex = 2;
-        // 
         // buttonQuit
         // 
         this.buttonQuit.Location = new Point(1069, 6);
@@ -75,18 +65,169 @@ partial class AdminMainMenu {
         this.buttonQuit.UseVisualStyleBackColor = true;
         this.buttonQuit.Click += this.ButtonQuit_Click;
         // 
+        // topBarPanel
+        // 
+        this.topBarPanel.Dock = DockStyle.Top;
+        this.topBarPanel.Location = new Point(0, 0);
+        this.topBarPanel.Name = "topBarPanel";
+        this.topBarPanel.Size = new Size(1184, 40);
+        this.topBarPanel.TabIndex = 1;
+        // 
+        // mainPanel
+        // 
+        this.mainPanel.ColumnCount = 5;
+        this.mainPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+        this.mainPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+        this.mainPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+        this.mainPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+        this.mainPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+        this.mainPanel.Controls.Add(this.addressManagementPanel, 1, 0);
+        this.mainPanel.Dock = DockStyle.Fill;
+        this.mainPanel.Location = new Point(0, 40);
+        this.mainPanel.Name = "mainPanel";
+        this.mainPanel.RowCount = 2;
+        this.mainPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+        this.mainPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+        this.mainPanel.Size = new Size(1184, 781);
+        this.mainPanel.TabIndex = 2;
+        // 
+        // addressManagementPanel
+        // 
+        this.addressManagementPanel.Controls.Add(this.addressMgmtMainPanel);
+        this.addressManagementPanel.Controls.Add(this.addressMgmtTopBar);
+        this.addressManagementPanel.Dock = DockStyle.Fill;
+        this.addressManagementPanel.Location = new Point(239, 3);
+        this.addressManagementPanel.Name = "addressManagementPanel";
+        this.addressManagementPanel.Padding = new Padding(10);
+        this.addressManagementPanel.Size = new Size(230, 384);
+        this.addressManagementPanel.TabIndex = 0;
+        // 
+        // addressMgmtMainPanel
+        // 
+        this.addressMgmtMainPanel.Controls.Add(this.deleteAddressButton);
+        this.addressMgmtMainPanel.Controls.Add(this.editAddressButton);
+        this.addressMgmtMainPanel.Controls.Add(this.viewAddressButton);
+        this.addressMgmtMainPanel.Controls.Add(this.filteredAddresses);
+        this.addressMgmtMainPanel.Controls.Add(this.addressSearchTextBox);
+        this.addressMgmtMainPanel.Controls.Add(this.existingAddressesLabel);
+        this.addressMgmtMainPanel.Controls.Add(this.createAddressButton);
+        this.addressMgmtMainPanel.Dock = DockStyle.Fill;
+        this.addressMgmtMainPanel.Location = new Point(10, 50);
+        this.addressMgmtMainPanel.Name = "addressMgmtMainPanel";
+        this.addressMgmtMainPanel.Size = new Size(210, 324);
+        this.addressMgmtMainPanel.TabIndex = 1;
+        // 
+        // existingAddressesLabel
+        // 
+        this.existingAddressesLabel.Dock = DockStyle.Top;
+        this.existingAddressesLabel.Location = new Point(0, 35);
+        this.existingAddressesLabel.Name = "existingAddressesLabel";
+        this.existingAddressesLabel.Size = new Size(210, 23);
+        this.existingAddressesLabel.TabIndex = 1;
+        this.existingAddressesLabel.Text = "Adresses existantes:";
+        this.existingAddressesLabel.TextAlign = ContentAlignment.MiddleCenter;
+        // 
+        // createAddressButton
+        // 
+        this.createAddressButton.Dock = DockStyle.Top;
+        this.createAddressButton.Location = new Point(0, 0);
+        this.createAddressButton.Name = "createAddressButton";
+        this.createAddressButton.Size = new Size(210, 35);
+        this.createAddressButton.TabIndex = 0;
+        this.createAddressButton.Text = "Nouvelle Adresse";
+        this.createAddressButton.UseVisualStyleBackColor = true;
+        this.createAddressButton.Click += this.CreateAddressButton_Click;
+        // 
+        // addressMgmtTopBar
+        // 
+        this.addressMgmtTopBar.Controls.Add(this.label1);
+        this.addressMgmtTopBar.Dock = DockStyle.Top;
+        this.addressMgmtTopBar.Location = new Point(10, 10);
+        this.addressMgmtTopBar.Name = "addressMgmtTopBar";
+        this.addressMgmtTopBar.Size = new Size(210, 40);
+        this.addressMgmtTopBar.TabIndex = 0;
+        // 
+        // label1
+        // 
+        this.label1.Dock = DockStyle.Fill;
+        this.label1.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
+        this.label1.Location = new Point(0, 0);
+        this.label1.Name = "label1";
+        this.label1.Size = new Size(210, 40);
+        this.label1.TabIndex = 0;
+        this.label1.Text = "Gestion des Adresses";
+        this.label1.TextAlign = ContentAlignment.MiddleCenter;
+        // 
+        // addressSearchTextBox
+        // 
+        this.addressSearchTextBox.Dock = DockStyle.Top;
+        this.addressSearchTextBox.Location = new Point(0, 58);
+        this.addressSearchTextBox.Name = "addressSearchTextBox";
+        this.addressSearchTextBox.Size = new Size(210, 23);
+        this.addressSearchTextBox.TabIndex = 2;
+        this.addressSearchTextBox.TextChanged += this.AddressSearchTextBox_TextChanged;
+        // 
+        // filteredAddresses
+        // 
+        this.filteredAddresses.Dock = DockStyle.Top;
+        this.filteredAddresses.FormattingEnabled = true;
+        this.filteredAddresses.ItemHeight = 15;
+        this.filteredAddresses.Location = new Point(0, 81);
+        this.filteredAddresses.Name = "filteredAddresses";
+        this.filteredAddresses.Size = new Size(210, 94);
+        this.filteredAddresses.TabIndex = 3;
+        this.filteredAddresses.SelectedIndexChanged += this.FilteredAddresses_SelectedIndexChanged;
+        // 
+        // viewAddressButton
+        // 
+        this.viewAddressButton.Dock = DockStyle.Top;
+        this.viewAddressButton.Location = new Point(0, 175);
+        this.viewAddressButton.Name = "viewAddressButton";
+        this.viewAddressButton.Size = new Size(210, 35);
+        this.viewAddressButton.TabIndex = 4;
+        this.viewAddressButton.Text = "Voir Détails";
+        this.viewAddressButton.UseVisualStyleBackColor = true;
+        this.viewAddressButton.Click += this.ViewAddressButton_Click;
+        // 
+        // editAddressButton
+        // 
+        this.editAddressButton.Dock = DockStyle.Top;
+        this.editAddressButton.Location = new Point(0, 210);
+        this.editAddressButton.Name = "editAddressButton";
+        this.editAddressButton.Size = new Size(210, 35);
+        this.editAddressButton.TabIndex = 5;
+        this.editAddressButton.Text = "Modifier l'Adresse";
+        this.editAddressButton.UseVisualStyleBackColor = true;
+        this.editAddressButton.Click += this.EditAddressButton_Click;
+        // 
+        // deleteAddressButton
+        // 
+        this.deleteAddressButton.Dock = DockStyle.Top;
+        this.deleteAddressButton.Location = new Point(0, 245);
+        this.deleteAddressButton.Name = "deleteAddressButton";
+        this.deleteAddressButton.Size = new Size(210, 35);
+        this.deleteAddressButton.TabIndex = 6;
+        this.deleteAddressButton.Text = "Supprimer l'Adresse";
+        this.deleteAddressButton.UseVisualStyleBackColor = true;
+        this.deleteAddressButton.Click += this.DeleteAddressButton_Click;
+        // 
         // AdminMainMenu
         // 
         this.AutoScaleDimensions = new SizeF(7F, 15F);
         this.AutoScaleMode = AutoScaleMode.Font;
         this.ClientSize = new Size(1184, 861);
-        this.Controls.Add(this.tableLayoutPanel1);
+        this.Controls.Add(this.mainPanel);
         this.Controls.Add(this.topBarPanel);
         this.Controls.Add(this.bottomBarPanel);
         this.Name = "AdminMainMenu";
         this.Text = "Menu Administration";
         this.WindowState = FormWindowState.Maximized;
         this.bottomBarPanel.ResumeLayout(false);
+        this.mainPanel.ResumeLayout(false);
+        this.addressManagementPanel.ResumeLayout(false);
+        this.addressMgmtMainPanel.ResumeLayout(false);
+        this.addressMgmtMainPanel.PerformLayout();
+        this.addressMgmtTopBar.ResumeLayout(false);
         this.ResumeLayout(false);
     }
 
@@ -95,5 +236,16 @@ partial class AdminMainMenu {
     private Panel bottomBarPanel;
     private Button buttonQuit;
     private Panel topBarPanel;
-    private TableLayoutPanel tableLayoutPanel1;
+    private TableLayoutPanel mainPanel;
+    private Panel addressManagementPanel;
+    private Panel addressMgmtMainPanel;
+    private Button createAddressButton;
+    private Panel addressMgmtTopBar;
+    private Label label1;
+    private Label existingAddressesLabel;
+    private TextBox addressSearchTextBox;
+    private Button viewAddressButton;
+    private ListBox filteredAddresses;
+    private Button deleteAddressButton;
+    private Button editAddressButton;
 }
--- Presentation/AdminMainMenu.cs
+++ Presentation/AdminMainMenu.cs
@@ -1,4 +1,6 @@
 ﻿using _420DA3_07451_Projet_Initial.Business.Abstracts;
+using _420DA3_07451_Projet_Initial.Business.Services;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,6 +19,71 @@ public partial class AdminMainMenu : Form {
         this.ParentFacade = parentFacade;
         this.InitializeComponent();
     }
+
+
+    #region Gestion des Adresses
+
+
+    private void AddressSearchTextBox_TextChanged(object sender, EventArgs e) {
+        this.filteredAddresses.DataSource = 
+            this.ParentFacade.GetService<AddressService>().SearchAddresses(this.addressSearchTextBox.Text);
+    }
+
+    private void ActivateAddressButtons() {
+        this.editAddressButton.Enabled = true;
+        this.viewAddressButton.Enabled = true;
+        this.deleteAddressButton.Enabled = true;
+    }
+
+    private void DeactivateAddressButtons() {
+        this.editAddressButton.Enabled = false;
+        this.viewAddressButton.Enabled = false;
+        this.deleteAddressButton.Enabled = false;
+    }
+
+    private void CreateAddressButton_Click(object sender, EventArgs e) {
+        _ = this.ParentFacade.GetService<AddressService>().CreateNewDtoInstance();
+    }
+
+    private void ViewAddressButton_Click(object sender, EventArgs e) {
+        if (this.filteredAddresses.SelectedItem == null) {
+            _ = MessageBox.Show("Pas d'adresse sélectionnée.");
+        } else {
+            _ = this.ParentFacade.GetService<AddressService>().DisplayDtoInstance((AddressDTO) this.filteredAddresses.SelectedItem);
+        }
+    }
+
+    private void EditAddressButton_Click(object sender, EventArgs e) {
+        if (this.filteredAddresses.SelectedItem == null) {
+            _ = MessageBox.Show("Pas d'adresse sélectionnée.");
+        } else {
+            _ = this.ParentFacade.GetService<AddressService>().UpdateDtoInstance((AddressDTO) this.filteredAddresses.SelectedItem);
+        }
+
+    }
+
+    private void DeleteAddressButton_Click(object sender, EventArgs e) {
+        if (this.filteredAddresses.SelectedItem == null) {
+            _ = MessageBox.Show("Pas d'adresse sélectionnée.");
+        } else {
+            _ = this.ParentFacade.GetService<AddressService>().DeleteDtoInstance((AddressDTO) this.filteredAddresses.SelectedItem);
+        }
+
+    }
+
+    private void FilteredAddresses_SelectedIndexChanged(object sender, EventArgs e) {
+        if (this.filteredAddresses.SelectedItem != null) { 
+            this.ActivateAddressButtons();
+        } else {
+            this.DeactivateAddressButtons();
+        }
+    }
+
+
+
+    #endregion
+
+
 
     private void ButtonQuit_Click(object sender, EventArgs e) {
         this.ParentFacade.ShutdownParentApplication();
