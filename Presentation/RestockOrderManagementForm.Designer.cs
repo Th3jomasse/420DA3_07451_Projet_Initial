a1ea3f4926d01d7c9ddd0a011f4d8102cf7e2437
Marc-Eric Boury <meboury@hotmail.com>
2023-12-20 01:19:59 -0500

    restock order form

--- Presentation/RestockOrderManagementForm.Designer.cs
+++ Presentation/RestockOrderManagementForm.Designer.cs
@@ -24,11 +24,270 @@ partial class RestockOrderManagementForm {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.topBarPanel = new Panel();
+        this.headerLabel = new Label();
+        this.bottomBarPanel = new Panel();
+        this.actionButton = new Button();
+        this.cancelButton = new Button();
+        this.mainTableLayoutPanel = new TableLayoutPanel();
+        this.mainPanel = new Panel();
+        this.entrepotSelector = new ComboBox();
+        this.statusSelector = new ComboBox();
+        this.quantityNumUpDown = new NumericUpDown();
+        this.produitLabel = new Label();
+        this.productSearchListBox = new ListBox();
+        this.productSearchFilter = new TextBox();
+        this.statutLabel = new Label();
+        this.quantiteLabel = new Label();
+        this.productLabel = new Label();
+        this.warehouseLabel = new Label();
+        this.idNumUpDown = new NumericUpDown();
+        this.idLabel = new Label();
+        this.topBarPanel.SuspendLayout();
+        this.bottomBarPanel.SuspendLayout();
+        this.mainTableLayoutPanel.SuspendLayout();
+        this.mainPanel.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize) this.quantityNumUpDown).BeginInit();
+        ((System.ComponentModel.ISupportInitialize) this.idNumUpDown).BeginInit();
+        this.SuspendLayout();
+        // 
+        // topBarPanel
+        // 
+        this.topBarPanel.Controls.Add(this.headerLabel);
+        this.topBarPanel.Dock = DockStyle.Top;
+        this.topBarPanel.Location = new Point(0, 0);
+        this.topBarPanel.Name = "topBarPanel";
+        this.topBarPanel.Size = new Size(484, 40);
+        this.topBarPanel.TabIndex = 0;
+        // 
+        // headerLabel
+        // 
+        this.headerLabel.Dock = DockStyle.Fill;
+        this.headerLabel.Font = new Font("Segoe UI Black", 15.75F,  FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+        this.headerLabel.Location = new Point(0, 0);
+        this.headerLabel.Name = "headerLabel";
+        this.headerLabel.Size = new Size(484, 40);
+        this.headerLabel.TabIndex = 2;
+        this.headerLabel.Text = "Gestion de commande de restockage";
+        this.headerLabel.TextAlign = ContentAlignment.MiddleCenter;
+        // 
+        // bottomBarPanel
+        // 
+        this.bottomBarPanel.Controls.Add(this.actionButton);
+        this.bottomBarPanel.Controls.Add(this.cancelButton);
+        this.bottomBarPanel.Dock = DockStyle.Bottom;
+        this.bottomBarPanel.Location = new Point(0, 321);
+        this.bottomBarPanel.Name = "bottomBarPanel";
+        this.bottomBarPanel.Size = new Size(484, 40);
+        this.bottomBarPanel.TabIndex = 1;
+        // 
+        // actionButton
+        // 
+        this.actionButton.Anchor =  AnchorStyles.Bottom | AnchorStyles.Right;
+        this.actionButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+        this.actionButton.Location = new Point(262, 6);
+        this.actionButton.Name = "actionButton";
+        this.actionButton.Size = new Size(129, 23);
+        this.actionButton.TabIndex = 1;
+        this.actionButton.Text = "ACTION";
+        this.actionButton.UseVisualStyleBackColor = true;
+        this.actionButton.Click += this.ActionButton_Click;
+        // 
+        // cancelButton
+        // 
+        this.cancelButton.Anchor =  AnchorStyles.Bottom | AnchorStyles.Right;
+        this.cancelButton.Location = new Point(397, 6);
+        this.cancelButton.Name = "cancelButton";
+        this.cancelButton.Size = new Size(75, 23);
+        this.cancelButton.TabIndex = 0;
+        this.cancelButton.Text = "Annuler";
+        this.cancelButton.UseVisualStyleBackColor = true;
+        this.cancelButton.Click += this.CancelButton_Click;
+        // 
+        // mainTableLayoutPanel
+        // 
+        this.mainTableLayoutPanel.ColumnCount = 3;
+        this.mainTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.mainTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 350F));
+        this.mainTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.mainTableLayoutPanel.Controls.Add(this.mainPanel, 1, 0);
+        this.mainTableLayoutPanel.Dock = DockStyle.Fill;
+        this.mainTableLayoutPanel.Location = new Point(0, 40);
+        this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+        this.mainTableLayoutPanel.RowCount = 1;
+        this.mainTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.mainTableLayoutPanel.Size = new Size(484, 281);
+        this.mainTableLayoutPanel.TabIndex = 2;
+        // 
+        // mainPanel
+        // 
+        this.mainPanel.Controls.Add(this.entrepotSelector);
+        this.mainPanel.Controls.Add(this.statusSelector);
+        this.mainPanel.Controls.Add(this.quantityNumUpDown);
+        this.mainPanel.Controls.Add(this.produitLabel);
+        this.mainPanel.Controls.Add(this.productSearchListBox);
+        this.mainPanel.Controls.Add(this.productSearchFilter);
+        this.mainPanel.Controls.Add(this.statutLabel);
+        this.mainPanel.Controls.Add(this.quantiteLabel);
+        this.mainPanel.Controls.Add(this.productLabel);
+        this.mainPanel.Controls.Add(this.warehouseLabel);
+        this.mainPanel.Controls.Add(this.idNumUpDown);
+        this.mainPanel.Controls.Add(this.idLabel);
+        this.mainPanel.Dock = DockStyle.Fill;
+        this.mainPanel.Location = new Point(70, 3);
+        this.mainPanel.Name = "mainPanel";
+        this.mainPanel.Padding = new Padding(10);
+        this.mainPanel.Size = new Size(344, 275);
+        this.mainPanel.TabIndex = 0;
+        // 
+        // entrepotSelector
+        // 
+        this.entrepotSelector.FormattingEnabled = true;
+        this.entrepotSelector.Location = new Point(139, 195);
+        this.entrepotSelector.Name = "entrepotSelector";
+        this.entrepotSelector.Size = new Size(192, 23);
+        this.entrepotSelector.TabIndex = 12;
+        // 
+        // statusSelector
+        // 
+        this.statusSelector.FormattingEnabled = true;
+        this.statusSelector.Location = new Point(139, 166);
+        this.statusSelector.Name = "statusSelector";
+        this.statusSelector.Size = new Size(192, 23);
+        this.statusSelector.TabIndex = 11;
+        // 
+        // quantityNumUpDown
+        // 
+        this.quantityNumUpDown.Location = new Point(139, 138);
+        this.quantityNumUpDown.Name = "quantityNumUpDown";
+        this.quantityNumUpDown.Size = new Size(192, 23);
+        this.quantityNumUpDown.TabIndex = 10;
+        // 
+        // produitLabel
+        // 
+        this.produitLabel.Location = new Point(13, 68);
+        this.produitLabel.Margin = new Padding(3, 0, 3, 5);
+        this.produitLabel.Name = "produitLabel";
+        this.produitLabel.Size = new Size(120, 23);
+        this.produitLabel.TabIndex = 9;
+        this.produitLabel.Text = "Produit:";
+        this.produitLabel.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // productSearchListBox
+        // 
+        this.productSearchListBox.FormattingEnabled = true;
+        this.productSearchListBox.ItemHeight = 15;
+        this.productSearchListBox.Location = new Point(139, 68);
+        this.productSearchListBox.Name = "productSearchListBox";
+        this.productSearchListBox.Size = new Size(192, 64);
+        this.productSearchListBox.TabIndex = 8;
+        this.productSearchListBox.SelectedIndexChanged += this.ProductSearchListBox_SelectedIndexChanged;
+        // 
+        // productSearchFilter
+        // 
+        this.productSearchFilter.Location = new Point(139, 39);
+        this.productSearchFilter.Name = "productSearchFilter";
+        this.productSearchFilter.Size = new Size(192, 23);
+        this.productSearchFilter.TabIndex = 7;
+        this.productSearchFilter.TextChanged += this.ProductSearchFilter_TextChanged;
+        // 
+        // statutLabel
+        // 
+        this.statutLabel.Location = new Point(13, 166);
+        this.statutLabel.Margin = new Padding(3, 0, 3, 5);
+        this.statutLabel.Name = "statutLabel";
+        this.statutLabel.Size = new Size(120, 23);
+        this.statutLabel.TabIndex = 5;
+        this.statutLabel.Text = "État:";
+        this.statutLabel.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // quantiteLabel
+        // 
+        this.quantiteLabel.Location = new Point(13, 138);
+        this.quantiteLabel.Margin = new Padding(3, 0, 3, 5);
+        this.quantiteLabel.Name = "quantiteLabel";
+        this.quantiteLabel.Size = new Size(120, 23);
+        this.quantiteLabel.TabIndex = 4;
+        this.quantiteLabel.Text = "Quantité:";
+        this.quantiteLabel.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // productLabel
+        // 
+        this.productLabel.Location = new Point(13, 38);
+        this.productLabel.Margin = new Padding(3, 0, 3, 5);
+        this.productLabel.Name = "productLabel";
+        this.productLabel.Size = new Size(120, 23);
+        this.productLabel.TabIndex = 3;
+        this.productLabel.Text = "Recherche produit:";
+        this.productLabel.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // warehouseLabel
+        // 
+        this.warehouseLabel.Location = new Point(13, 194);
+        this.warehouseLabel.Margin = new Padding(3, 0, 3, 5);
+        this.warehouseLabel.Name = "warehouseLabel";
+        this.warehouseLabel.Size = new Size(120, 23);
+        this.warehouseLabel.TabIndex = 2;
+        this.warehouseLabel.Text = "Entrepôt destinataire:";
+        this.warehouseLabel.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // idNumUpDown
+        // 
+        this.idNumUpDown.Enabled = false;
+        this.idNumUpDown.Location = new Point(139, 12);
+        this.idNumUpDown.Name = "idNumUpDown";
+        this.idNumUpDown.Size = new Size(192, 23);
+        this.idNumUpDown.TabIndex = 1;
+        // 
+        // idLabel
+        // 
+        this.idLabel.Location = new Point(13, 10);
+        this.idLabel.Margin = new Padding(3, 0, 3, 5);
+        this.idLabel.Name = "idLabel";
+        this.idLabel.Size = new Size(120, 23);
+        this.idLabel.TabIndex = 0;
+        this.idLabel.Text = "Id:";
+        this.idLabel.TextAlign = ContentAlignment.MiddleRight;
+        // 
+        // RestockOrderManagementForm
+        // 
+        this.AutoScaleDimensions = new SizeF(7F, 15F);
+        this.AutoScaleMode = AutoScaleMode.Font;
+        this.ClientSize = new Size(484, 361);
+        this.Controls.Add(this.mainTableLayoutPanel);
+        this.Controls.Add(this.bottomBarPanel);
+        this.Controls.Add(this.topBarPanel);
+        this.Name = "RestockOrderManagementForm";
         this.Text = "RestockOrderManagementForm";
+        this.topBarPanel.ResumeLayout(false);
+        this.bottomBarPanel.ResumeLayout(false);
+        this.mainTableLayoutPanel.ResumeLayout(false);
+        this.mainPanel.ResumeLayout(false);
+        this.mainPanel.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize) this.quantityNumUpDown).EndInit();
+        ((System.ComponentModel.ISupportInitialize) this.idNumUpDown).EndInit();
+        this.ResumeLayout(false);
     }
 
     #endregion
+
+    private Panel topBarPanel;
+    private Panel bottomBarPanel;
+    private TableLayoutPanel mainTableLayoutPanel;
+    private Panel mainPanel;
+    private Label warehouseLabel;
+    private NumericUpDown idNumUpDown;
+    private Label idLabel;
+    private Label productLabel;
+    private Label quantiteLabel;
+    private Label statutLabel;
+    private ComboBox entrepotSelector;
+    private ComboBox statusSelector;
+    private NumericUpDown quantityNumUpDown;
+    private Label produitLabel;
+    private ListBox productSearchListBox;
+    private TextBox productSearchFilter;
+    private Button actionButton;
+    private Button cancelButton;
+    private Label headerLabel;
 }
--- Presentation/RestockOrderManagementForm.cs
+++ Presentation/RestockOrderManagementForm.cs
@@ -1,6 +1,9 @@
 ﻿using _420DA3_07451_Projet_Initial.Business.Abstracts;
+using _420DA3_07451_Projet_Initial.Business.Services;
 using _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs.Enums;
 using _420DA3_07451_Projet_Initial.Presentation.Abstracts;
+using _420DA3_07451_Projet_Initial.Presentation.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,30 +16,143 @@ using System.Windows.Forms;
 
 namespace _420DA3_07451_Projet_Initial.Presentation;
 public partial class RestockOrderManagementForm : Form, IDtoManagementView<RestockOrderDTO> {
-    private AbstractFacade facade;
+    private readonly AbstractFacade facade;
+    private RestockOrderDTO workingInstance = null!;
+    private ViewIntentEnum viewIntent;
 
     public RestockOrderManagementForm(AbstractFacade facade) {
         this.facade = facade;
         this.InitializeComponent();
+        this.LoadWarehousesCombobox();
+        this.LoadStatusesCombobox();
     }
 
     public DialogResult OpenForCreation(RestockOrderDTO blankInstance) {
-        // TODO: (Prof) Complete form code
-        throw new NotImplementedException();
+        this.viewIntent = ViewIntentEnum.Creation;
+        this.actionButton.Text = "Créer!";
+        return this.OpenFor(blankInstance);
+
     }
 
     public DialogResult OpenForDeletion(RestockOrderDTO instance) {
-        // TODO: (Prof) Complete form code
-        throw new NotImplementedException();
+        _ = MessageBox.Show("Cette action n'est pas autorisée.");
+        return DialogResult.Cancel;
     }
 
     public DialogResult OpenForEdition(RestockOrderDTO instance) {
-        // TODO: (Prof) Complete form code
-        throw new NotImplementedException();
+        _ = MessageBox.Show("Cette action n'est pas autorisée.");
+        return DialogResult.Cancel;
     }
 
     public DialogResult OpenForVisualization(RestockOrderDTO instance) {
-        // TODO: (Prof) Complete form code
-        throw new NotImplementedException();
+        this.viewIntent = ViewIntentEnum.Visualization;
+        this.actionButton.Text = "OK";
+        return this.OpenFor(instance);
+    }
+
+    private DialogResult OpenFor(RestockOrderDTO instance) {
+        this.workingInstance = instance;
+        this.LoadInstanceDataInControls(instance);
+        switch (this.viewIntent) {
+            case ViewIntentEnum.Creation:
+                this.ActivateControls();
+                break;
+            case ViewIntentEnum.Visualization:
+                this.DeactivateControls();
+                break;
+            default:
+                throw new NotImplementedException("View intent is not supported.");
+        }
+        return this.ShowDialog();
+    }
+
+    private void ActivateControls() {
+        this.idNumUpDown.Enabled = false;
+        this.productSearchFilter.Enabled = true;
+        this.productSearchListBox.Enabled = true;
+        this.statusSelector.Enabled = true;
+        this.entrepotSelector.Enabled = false;
+        this.quantityNumUpDown.Enabled = true;
+    }
+
+    private void DeactivateControls() {
+        this.idNumUpDown.Enabled = false;
+        this.productSearchFilter.Enabled = false;
+        this.productSearchListBox.Enabled = false;
+        this.statusSelector.Enabled = false;
+        this.entrepotSelector.Enabled = false;
+        this.quantityNumUpDown.Enabled = false;
+    }
+
+    private void LoadInstanceDataInControls(RestockOrderDTO instance) {
+        this.idNumUpDown.Value = instance.Id;
+        if (!this.productSearchListBox.Items.Contains(instance.Produit)) {
+            _ = this.productSearchListBox.Items.Add(instance.Produit);
+        }
+        this.quantityNumUpDown.Value = instance.Quantity;
+        this.productSearchListBox.SelectedItem = instance.Produit;
+        this.statusSelector.SelectedItem = instance.Status;
+        this.entrepotSelector.SelectedItem = instance.DestinationWarehouse;
+    }
+
+    private void StoreControlsDataInInstance() {
+        this.workingInstance.Produit = (ProduitsDTO) this.productSearchListBox.SelectedItem;
+        this.workingInstance.Status = (RestockOrderStatusEnum) this.statusSelector.SelectedItem;
+        this.workingInstance.DestinationWarehouse = (EntrepotDTO) this.entrepotSelector.SelectedItem;
+        this.workingInstance.Quantity = (int) this.quantityNumUpDown.Value;
+    }
+
+    private void LoadWarehousesCombobox() {
+        this.entrepotSelector.DataSource = this.facade.GetService<EntrepotService>().GetAllEntrepots();
+    }
+
+    private void LoadStatusesCombobox() {
+        this.statusSelector.Items.Clear();
+        foreach (RestockOrderStatusEnum enumValue in Enum.GetValues(typeof(RestockOrderStatusEnum))) {
+            _ = this.statusSelector.Items.Add(enumValue);
+        }
+    }
+
+    private void ProductSearchFilter_TextChanged(object sender, EventArgs e) {
+        this.productSearchListBox.Items.Clear();
+        this.productSearchListBox.Items.AddRange(
+            this.facade.GetService<ProduitsService>().SearchProducts(this.productSearchFilter.Text).ToArray()
+            );
+    }
+
+    private void ProductSearchListBox_SelectedIndexChanged(object sender, EventArgs e) {
+        if (this.productSearchListBox.SelectedItem is not null) {
+            ProduitsDTO produitSelectionne = (ProduitsDTO) this.productSearchListBox.SelectedItem;
+            this.entrepotSelector.SelectedItem = produitSelectionne.ClientProprietaireProduit.AssignedWarehouse;
+        }
+    }
+
+    private bool ValidateControlsData() {
+        return this.productSearchListBox.SelectedItem is null
+            ? throw new Exception("Pas de produit sélectionné.")
+            : this.quantityNumUpDown.Value <= 0
+                ? throw new Exception("La quantité ne peut être 0 ou moins.")
+                : this.statusSelector.SelectedItem is null
+                    ? throw new Exception("Pas de statut sélectionné.")
+                    : this.entrepotSelector.SelectedItem is null 
+                        ? throw new Exception("Pas d'entrepôt sélectionné.") 
+                        : true;
+    }
+
+    private void CancelButton_Click(object sender, EventArgs e) {
+        this.DialogResult = DialogResult.Cancel;
+    }
+
+    private void ActionButton_Click(object sender, EventArgs e) {
+        if (this.ValidateControlsData()) {
+            switch (this.viewIntent) {
+                case ViewIntentEnum.Creation:
+                    this.StoreControlsDataInInstance();
+                    break;
+                default:
+                    break;
+            }
+            this.DialogResult = DialogResult.OK;
+        }
     }
 }
