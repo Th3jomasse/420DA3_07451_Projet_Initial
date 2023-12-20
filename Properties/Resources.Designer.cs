17be079782ab7f45a75dc79ec149003fe92075b0
Marc-Eric Boury <meboury@hotmail.com>
2023-12-20 03:41:16 -0500

    entrepot management form

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace _420DA3_07451_Projet_Initial.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap plus_20px {
+            get {
+                object obj = ResourceManager.GetObject("plus_20px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
